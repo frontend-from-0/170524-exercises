477a2404d50d23763880c444a88563ce3329c80a
sahraozdl <seviyyenozdil@gmail.com>
2024-09-24 10:57:16 +0300

    fixed ex11-ex12

--- lesson14/sahra/script.js
+++ lesson14/sahra/script.js
@@ -118,15 +118,16 @@ console.log('------------------------');
 
 // 11. Declare a variable named `numberEx11` and initialize it to -3.
 // Use the prefix ++ operator to increment the value of `numberEx11` by 1, then multiply the result by 2 and log it to the console.
-let numberEx11 = -3;
-var result = (numberEx11 + 1) * 2;
-console.log(result);
+const numberEx11 = -3;
+numberEx11++;
+let resultEx11 = (numberEx11 + 1) * 2;
+console.log(resultEx11);
 console.log('------------------------');
 
 // 12. Declare two variables named `a` and `b` and initialize them to 2 and 3, respectively.
 // Use the prefix ++ operator to increment the value of `a` by 1, then add `b` to the result and log it to the console.
-let a = 2;
-let b = 3;
+const a = 2;
+const b = 3;
 let result = (++a + b);
 
 console.log(result);
