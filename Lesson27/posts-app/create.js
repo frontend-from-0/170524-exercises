6e4c002de3982e33cca5f599406779d03141a25e
Anna S. A <anna.atalay@minna.tech>
2024-12-10 21:49:20 +0100

    Add Lesson27 files

---
+++ Lesson27/posts-app/create.js
@@ -0,0 +1,6 @@
+function createPost() {
+  // Get the form data
+  // Validate the form data
+  // If form data is not valid, show error messages
+  // If form data is valid, make an API request to create the post (POST request)
+}
