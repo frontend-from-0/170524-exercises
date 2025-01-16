66756e92ea740397955d10d90c76263a806cf57f
sahraozdl <seviyyenozdil@gmail.com>
2025-01-16 13:54:07 +0300

    Update sort.js

--- Lesson24/sahra/sort.js
+++ Lesson24/sahra/sort.js
@@ -16,8 +16,8 @@ console.log(alphabeticalOrder);
 // Sort an array of numbers in descending order.
 // Expected output: [100, 40, 25, 10, 5, 1]
 const scores = [40, 100, 1, 5, 25, 10];
-const sortNumbers=[...numbers].sort((a, b) => b-a);
-console.log(numbers,sortNumbers);
+const scoresInDescendingOrder=[...numbers].sort((a, b) => b-a);
+console.log(numbers,scoresInDescendingOrder);
 
 // Exercise 4: Sorting Objects
 // Sort an array of objects based on a specific property (e.g., age).
@@ -28,15 +28,15 @@ const people = [
   { name: "Gary", age: 35 },
   { name: "Ellen", age: 22 }
 ];
-const sortAge =[...people].sort((a,b) => a.age-b.age);
-console.log(people,sortAge);
+const peopleSortedByAge =[...people].sort((a,b) => a.age-b.age);
+console.log(people,peopleSortedByAge);
 // Exercise 5: Complex Sorting
 // Sort an array of strings by their length.
 // Expected output: ["pen", "book", "paper", "pencil", "notebook"]
 
 const words = ["notebook", "pen", "paper", "book", "pencil"];
-const sortLength =[...words].sort((a,b)=>a.length-b.length);
-console.log(words,sortLength);
+const sortedByLength =[...words].sort((a,b)=>a.length-b.length);
+console.log(words,sortedByLength);
 
 
 
