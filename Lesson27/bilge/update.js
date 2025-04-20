ffc62a8836de385846470c131d668c2282f839cd
bilge.ers <bilge.ersari@gmail.com>
2025-04-20 23:39:56 +0300

    created the files and started to work on them

---
+++ Lesson27/bilge/update.js
@@ -0,0 +1,22 @@
+const params = new URLSearchParams(window.location.search);
+
+const postId = params.get('id');
+
+console.log(params, postId);
+
+
+// TODO
+// Make API request to fetch the post with the given ID (Get request)
+
+fetch("https://jsonplaceholder.typicode.com/posts/id")
+    .then((response) => response.json())
+    .then((postId) => {
+      console.log(postId);
+    });
+
+// Prefill the form in update-post.html with the post title and body
+
+
+// When a user submits the form, validate the form data
+// If form data is not valid, show error messages
+// If form data is valid, make an API request to update the post (PUT request)
